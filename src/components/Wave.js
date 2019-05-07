909a0c491521fb5186945f0af6e27a52194e00bf
Eli Lerner <elilerner@Elis-MacBook-Pro.local>
2019-05-07 09:11:30 -0400

    Cell Group

--- src/components/Wave.js
+++ src/components/Wave.js
@@ -8,7 +8,7 @@ const Wave = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path fill="white">
+    <path fill="#202B4F">
       <animate
         repeatCount="indefinite"
         fill="freeze"
