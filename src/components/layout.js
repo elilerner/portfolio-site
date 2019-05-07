aa52ae45aca982b6c5c1edde4025db36a3f2aa5b
Eli Lerner <elilerner@Elis-MacBook-Pro.local>
2019-05-07 10:21:44 -0400

    Footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import Footer from "./Footer"
 
 import Header from "./header"
 import "./layout.css"
@@ -27,6 +28,7 @@ const Layout = ({ children }) => (
       <>
         <Header />
         <main>{children}</main>
+        <Footer />
       </>
     )}
   />
