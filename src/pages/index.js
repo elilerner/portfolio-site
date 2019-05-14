0717bc8c90386ac7c043749d825c4bfd03b1bce4
Eli Lerner <elilerner@Elis-MacBook-Pro.local>
2019-05-13 21:50:05 -0400

    title and card hover

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = () => (
         <h1 id="mainTitle">
           I'm Eli Lerner <br />
         </h1>
-        <p id="mainText">A Designer & Front-End Developer</p>
+        <p id="mainText">A Full-Stack Developer</p>
         <a href="https://zety.com/mycv/elilerner">View My Online Resume</a>
         <div className="Logos">
           <img src={require("../images/logo-sketch.png")} width="50" />
